33a59b26d462a5ce927ff48f2d17fe777eb6adc4
AtharvNerkar <atharvnnitb@gmail.com>
2025-06-28 16:12:20 +0530

    commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ const connectDB = async ()=>{
     try {
         mongoose.connection.on('connected',()=>console.log("Database Connected"));
         
-        await mongoose.connect(`${process.env.MONGODB_URI}/Hotel Booking`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/Hotel-Booking`)
     } catch (error) {
         console.log(error.message);
         
--- server/server.js
+++ server/server.js
@@ -20,4 +20,4 @@ app.get('/', (req,res)=> res.send("API is working"))
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)); 
